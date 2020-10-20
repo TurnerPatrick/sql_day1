b807707691732d977faf9e1480245812b07e472a
TurnerPatrick <Pturner31@gmail.com>
2020-10-20 06:58:48 -0500

    completed homework

--- sql-day_1.sql
+++ sql-day_1.sql
@@ -204,11 +204,11 @@ WHERE last_name LIKE '__%es' AND store_id = 1;
 -- Question 9
 -- How many payment amounts (4.99, 5.99, etc.) had a number of rentals above 250 for customers
 -- with ids between 380 and 430? (use group by and having > 250)
-SELECT *
+SELECT amount, COUNT(rental_id)
 FROM payment;
 WHERE customer_id BETWEEN 380 and 430
 GROUP by amount
-HAVING COUNT(rental_id) > 250;
+HAVING COUNT(rental_id);
 
 -- Question 10
 -- Within the film table, how many rating categories are there? And what rating has the most
